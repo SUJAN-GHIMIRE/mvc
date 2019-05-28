d33f78906a88074a43516b9c5c6bfdb970cee1e6
SUJAN <mesujanghimire@gmail.com>
2019-05-28 08:01:00 +0545

    Adding ViewModels
    the list of customer is printed in the view by using the viewmodel class

--- project_y/Controllers/MovieController.cs
+++ project_y/Controllers/MovieController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using project_y.Models;
+using project_y.ViewModels;
 
 namespace project_y.Controllers
 {
@@ -12,8 +13,24 @@ namespace project_y.Controllers
         // GET: Movie/Random
         public ActionResult Random()
         {
-            var movie = new Movie() {name = "Batman"};
-            return View(movie);
+            var movie = new Movie()
+            {
+                name = "Batman"
+            };
+            var customer = new List<Customer>
+            {
+                new Customer{name = "customer 1"},
+                new Customer{name = "Customer 2"},
+                new Customer{name = "sujan"},
+                new Customer{name = "bulbul pandey"}
+            };
+
+            var viewmodel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customer
+            };
+            return View(viewmodel);
         }
 
         [Route("movie/released/{year:regex(\\d{4})}/{month:regex(\\d{2}):range(1,12)}")]
@@ -21,5 +38,8 @@ namespace project_y.Controllers
         {
             return Content(year + "/" + month);
         }
+
+
+
     }
 }
---
+++ project_y/ViewModels/RandomMovieViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using project_y.Models;
+
+namespace project_y.ViewModels
+{
+    public class RandomMovieViewModel
+    {
+        public Movie Movie { get; set; }
+        public List<Customer> Customers { get; set; }
+    }
+}
