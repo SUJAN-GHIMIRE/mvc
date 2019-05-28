f7fdd3c14931c552ae52dea2b8d0492d259d111a
SUJAN <mesujanghimire@gmail.com>
2019-05-28 06:15:10 +0545

    first setup commit
    added a movie.cs in model,a controller class MovieController.cs in controller, and added a view in a automatically created folder called movie

---
+++ project_y/Controllers/MovieController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using project_y.Models;
+
+namespace project_y.Controllers
+{
+    public class MovieController : Controller
+    {
+        // GET: Movie/Random
+        public ActionResult Random()
+        {
+            var movie = new Movie() {name = "Batman"};
+            return View(movie);
+        }
+    }
+}
